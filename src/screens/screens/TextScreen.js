068bed7b29171b99abaf95dda29a94047d8dcbb4
GayanS <emailziggi@gmail.com>
2024-10-10 16:30:23 -0700

    Text update

--- src/screens/screens/TextScreen.js
+++ src/screens/screens/TextScreen.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
+  const [name, setName] = useState("");
+  const [password, setPass] = useState("");
+
   return (
     <View>
       <Text>Enter Name</Text>
@@ -9,7 +12,20 @@ const TextScreen = () => {
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
+        onChangeText={(newValue) => setName(newValue)}
       />
+      <Text>Welcome {name}. Good Afternoon!</Text>
+
+      <Text>Enter a pasword</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={(newPass) => setPass(newPass)}
+      />
+      {password.length < 5 ? (
+        <Text>The password neededs to be more than 5 charactors</Text>
+      ) : null}
     </View>
   );
 };
