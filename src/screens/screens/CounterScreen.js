0663dc2c14b80d468533fb03fc95e865889ef963
GayanS <emailziggi@gmail.com>
2024-10-09 19:08:03 -0700

    Using reducer

--- src/screens/screens/CounterScreen.js
+++ src/screens/screens/CounterScreen.js
@@ -1,3 +1,48 @@
+import React, { useReducer } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+const reducer = (state, action) => {
+  //state === {count : number}
+  //action === {type: 'increment' || 'decrement', paylord: 1 }
+
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + action.paylord };
+    case "decrement":
+      return { ...state, count: state.count - action.paylord };
+    default:
+      return state;
+  }
+};
+
+const CounterScreen = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  return (
+    <View>
+      <Button
+        title="Increase"
+        onPress={() => {
+          dispatch({ type: "increment", paylord: 1 });
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          dispatch({ type: "decrement", paylord: 1 });
+        }}
+      />
+      <Text>Cunrrent counter : {state.count}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
+
+/* -------------------
+
 import React, { useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
@@ -26,3 +71,7 @@ const CounterScreen = () => {
 const styles = StyleSheet.create({});
 
 export default CounterScreen;
+
+
+
+---------------------- */
--- src/screens/screens/EditcolorScreen.js
+++ src/screens/screens/EditcolorScreen.js
@@ -10,34 +10,55 @@ const reducer = (state, action) => {
 
   switch (action.colorToChange) {
     case "red":
-      return { ...state, red: state.red + action.amount };
+      return state.red + action.amount > 255 || state.red + action.amount < 0
+        ? state
+        : { ...state, red: state.red + action.amount };
     case "green":
-      return { ...state, green: state.green + action.amount };
+      return state.green + action.amount > 255 ||
+        state.green + action.amount < 0
+        ? state
+        : { ...state, green: state.green + action.amount };
     case "blue":
-      return { ...state, blue: state.blue + action.amount };
+      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        ? state
+        : { ...state, blue: state.blue + action.amount };
     default:
       return state;
   }
 };
 
 const EditcolorScreen = () => {
+  // dispatch === run my reducer
   const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+  const { red, green, blue } = state;
 
   return (
     <View>
       <ColorCorrection
-        onIncrease={() => {}}
-        onDecrease={() => {}}
+        onIncrease={() =>
+          dispatch({ colorToChange: "red", amount: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: "red", amount: -1 * COLOR_INCREMENT })
+        }
         color="Red"
       />
       <ColorCorrection
-        onIncrease={() => {}}
-        onDecrease={() => {}}
+        onIncrease={() =>
+          dispatch({ colorToChange: "green", amount: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: "green", amount: -1 * COLOR_INCREMENT })
+        }
         color="Green"
       />
       <ColorCorrection
-        onIncrease={() => {}}
-        onDecrease={() => {}}
+        onIncrease={() =>
+          dispatch({ colorToChange: "blue", amount: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: "blue", amount: -1 * COLOR_INCREMENT })
+        }
         color="Blue"
       />
 
